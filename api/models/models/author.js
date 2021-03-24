79179c96697be24a6ce089d7d9cfeda4a5b8943c
Juliana Amoasei <juliana.amoasei@gmail.com>
2021-03-23 23:42:19 -0300

    associacoes 1:n

--- api/models/models/author.js
+++ api/models/models/author.js
@@ -4,7 +4,10 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Author extends Model {
     static associate(models) {
-      // define association here
+      // as associações são feitas nas duas "pontas" do 1-muitos. Documentação do Sequelize sobre Associações: https://sequelize.org/master/manual/assocs.html#one-to-many-relationships
+      Author.hasMany(models.Book, {
+        foreignKey: 'author_id'
+      })
     }
   };
   Author.init({
--- api/models/models/book.js
+++ api/models/models/book.js
@@ -1,17 +1,18 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Book extends Model {
     static associate(models) {
-      // define association here
+      // as associações são feitas nas duas "pontas" do 1-muitos. Documentação do Sequelize sobre Associações: https://sequelize.org/master/manual/assocs.html#one-to-many-relationships
+      Book.belongsTo(models.Author, {
+        foreignKey: 'author_id'
+      })
     }
   };
   Book.init({
     title: DataTypes.STRING,
     author_id: DataTypes.INTEGER
-
   }, {
     sequelize,
     modelName: 'Book',
